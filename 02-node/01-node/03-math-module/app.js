f2d8916633c918cda9a8c0c60efc1eabe31d4f7c
Madelyn Koch <madelynkoch@gmail.com>
2019-03-21 13:24:55 -0700

    add mathlib

---
+++ 02-node/01-node/03-math-module/app.js
@@ -0,0 +1,9 @@
+var mathlib = require('./mathlib.js')();
+
+console.log(mathlib.add(4,5));
+
+console.log(mathlib.multiply(4,5));
+
+console.log(mathlib.square(4));
+
+console.log(mathlib.random(1,20));
