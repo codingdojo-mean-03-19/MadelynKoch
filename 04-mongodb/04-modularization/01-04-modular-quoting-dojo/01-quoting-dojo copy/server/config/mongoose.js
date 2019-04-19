717be03b23349dd750b382005d90d2a2245992b1
Madelyn Koch <madelynkoch@gmail.com>
2019-04-18 19:01:30 -0700

    add bonus challenge

--- 04-mongodb/04-modularization/01-04-modular-quoting-dojo/01-quoting-dojo copy/server/config/mongoose.js
+++ 04-mongodb/04-modularization/01-04-modular-quoting-dojo/01-quoting-dojo copy/server/config/mongoose.js
@@ -1,4 +1,14 @@
 const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
 mongoose.connect('mongodb://localhost/quoting_dojo');
+
+var models_path = path.join(__dirname, './../models');
+
+fs.readdirSync(models_path).forEach(function(file) {
+  if(file.indexOf('.js') >= 0) {
+    require(models_path + '/' + file);
+   }
+})
 
 module.exports = mongoose;
